ba4d4badb7fd83943cc2893d06257becb6b7da21
Vitor Nicchio Alves <vitornicchioalves@gmail.com>
2020-04-21 20:55:31 -0300

    feat: create users

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import 'reflect-metadata';
 import express from 'express';
 
 import './database';
+import routes from './routes';
 
 const app = express();
+app.use(routes);
 
 app.get('/', (request, response) => response.json({ ok: true }));
 
