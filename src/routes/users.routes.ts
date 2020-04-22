dda89c94996fef403ab64e3dcf7cac97ffc8ce28
Vitor Nicchio Alves <vitornicchioalves@gmail.com>
2020-04-21 21:17:24 -0300

    feat: add avatar to user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
@@ -14,5 +18,22 @@ usersRouter.post('/', async (request, response) => {
 
   return response.json(user);
 });
+
+usersRouter.patch(
+  '/avatar',
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: 'd3facdbf-ed2e-41d7-be45-0fa11b063034',
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  },
+);
 
 export default usersRouter;
