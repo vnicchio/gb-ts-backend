03a7868daef265b821a6a2dd06d970c916d84d5c
Vitor Nicchio Alves <vitornicchioalves@gmail.com>
2020-05-13 21:33:42 -0300

    configuring MongoDB

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import Appointment from '../infra/typeorm/entities/Appointment';
 
 interface IRequest {
   provider_id: string;
